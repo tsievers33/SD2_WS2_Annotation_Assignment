46c6c11b06406746c2b7b15ed44273ae522ecd38
Tom Sievers <t.sievers@windesheim.nl>
2023-04-25 08:36:29 +0200

    Init MVC ASP.Net project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SD2_WS2_Annotation_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,6 @@
+﻿namespace SD2_WS2_Annotation_Assignment.Models
+{
+    public class User
+    {
+    }
+}
